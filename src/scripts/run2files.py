1035b42c90508fe5451794e8b0f84cf216bdfa18
Chris Villalta <cfvillalta@gmail.com>
2017-11-11 09:17:52 -0800

    changed file limit to 1 million

--- src/scripts/run2files.py
+++ src/scripts/run2files.py
@@ -30,7 +30,7 @@ class Runs:
     logging.basicConfig()
 
     @staticmethod
-    def __get_files_to_download(myAPI, runId, fileLimit=1024):
+    def __get_files_to_download(myAPI, runId, fileLimit=10000000):
         return myAPI.getRunFilesById(Id=runId, queryPars=qp({'Limit' : fileLimit}))
 
     @staticmethod
