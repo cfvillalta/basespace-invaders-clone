2bca09eb6605f2d2c926b2c6dca1fad07b2b817d
Chris Villalta <cfvillalta@gmail.com>
2017-11-14 09:11:08 -0800

    less printing

--- src/scripts/run2files.py
+++ src/scripts/run2files.py
@@ -97,9 +97,9 @@ class Runs:
 
         for runFile in runFiles[int(n1):int(n2)]:
             outDir = os.path.join(outputDirectory, expName)
-            print 'Downloading (%d/%d): %s' % ((i+1), numFiles, str())
-            print "BaseSpace File Path: %s" % runFile.Path
-            print "Destination File Path: %s" % os.path.join(outDir, runFile.Name)
+            #print 'Downloading (%d/%d): %s' % ((i+1), numFiles, str())
+            #print "BaseSpace File Path: %s" % runFile.Path
+            #print "Destination File Path: %s" % os.path.join(outDir, runFile.Name)
             if not options.dryRun:
                 runFile.downloadFile(myAPI, outDir, createBsDir=createBsDir)
             i = i + 1
